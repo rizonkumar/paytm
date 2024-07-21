b2fb7eb6e3bb908e46634d9521bc4d0f272900b8
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-21 21:06:35 +0530

    Feat: Added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const mainRouter = require("./routes/index")
+const cors = require("cors");
+
 
 const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+const mainRouter = require("./routes/index")
 
 const PORT = process.env.PORT || 5000;
 console.log(PORT, "PORT")
