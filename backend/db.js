7f4079c02a62b39c02f0bfe2da3d3a65a033d3ed
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-21 22:33:08 +0530

    Feat: Account Schema

--- backend/db.js
+++ backend/db.js
@@ -40,8 +40,22 @@ const userSchema = mongoose.Schema({
   },
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, //Reference to User Model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-  User: User,
+  User,
+  Account
 };
