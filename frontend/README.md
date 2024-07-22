f00f14c1592b974ed753e4e9c90ee9a6499663a7
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-22 12:29:32 +0530

    Feat: Implemented Users && Button &&  SendMoney  && Signup

--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Paytm
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### To Do
+    - /me endpoint that return the basically the user is logged in or not, only the backend will tell you that
+    - Whenever the use hit the end point we will always /me endpoint to get back the user is logged in or not and if it's logged in go to the /dashboard and if'ts not then go to the signup page
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -4,6 +4,20 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios.get("http://localhost:3001/api/v1/user/bulk", {
+      params: { filter },
+      headers: { Authorization: `Bearer ${token}` }
+    }).then((response) => {
+      setUsers(response.data.users);
+      console.log("users", response.data.users);
+    });
+  }, [filter]);
+
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
@@ -12,9 +26,14 @@ export const Users = () => {
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-200"
-        ></input>
+          onChange={(e) => setFilter(e.target.value)}
+        />
       </div>
-      <div>{/* {users.map(user => <User user={user} />)} */}</div>
+      <div>
+        {users.map((user) => (
+          <User key={user._id} user={user} />
+        ))}
+      </div>
     </>
   );
 };
@@ -23,20 +42,19 @@ function User({ user }) {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between my-2 p-2 border rounded border-slate-200">
       <div className="flex">
-        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-          <div className="flex flex-col justify-center h-full text-xl">
-            {/* {user.firstName[0]} */}
+        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center items-center mr-2">
+          <div className="text-xl">{user.firstName[0]}</div>
+        </div>
+        <div className="flex flex-col justify-center">
+          <div>
+            {user.firstName} {user.lastName}
           </div>
         </div>
-        <div className="flex flex-col justify-center h-ful">
-          <div>{/* {user.firstName} {user.lastName} */}</div>
-        </div>
       </div>
-
-      <div className="flex flex-col justify-center h-ful">
-        <Button label={"Send Money"} />
+      <div className="flex flex-col justify-center">
+        <Button label={"Send Money"} onClick={() => navigate("/send?id=" + user._id + "&name=" + user.firstName)} />
       </div>
     </div>
   );
